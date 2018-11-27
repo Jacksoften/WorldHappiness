d96dd976a68c7895bce3ffbc4d937e67b1d730f7
Jack Li <liyunzhejack@gmail.com>
2018-11-26 16:18:22 -0800

    two-way ANOVA

--- EDA.R
+++ EDA.R
@@ -1,22 +1,22 @@
 # Data exploration
 
 # default directory is at finalProject where data are saved
-data1 = read.csv("/data/2015.csv")
-data2 = read.csv("/data/2016.csv")
-data3 = read.csv("/data/2017.csv")
+data1 = read.csv("./data/2015.csv")
+data2 = read.csv("./data/2016.csv")
+data3 = read.csv("./data/2017.csv")
 
 # check the number of counties in each region
-aggregate(Country ~ Region, data = data1, length)
+ncountries_per_region = aggregate(Country ~ Region, data = data1, length)
 
 # parallel boxplots of six factors according to region
 # We want to know if six factors behave differently 
 #  across regions
-boxplot(Economy..GDP.per.Capita. ~ Region, data=data1)
-boxplot(Family ~ Region, data=data1)
-boxplot(Freedom ~ Region, data=data1)
-boxplot(Generosity ~ Region, data=data1)
-boxplot(Health..life.Expectancy ~ Region, data=data1)
-boxplot(Trust..Government.Corruption. ~ Region, data=data1)
+p1 = boxplot(Economy..GDP.per.Capita. ~ Region, data=data1)
+p2 = boxplot(Family ~ Region, data=data1)
+p3 = boxplot(Freedom ~ Region, data=data1)
+p4 = boxplot(Generosity ~ Region, data=data1)
+p5 = boxplot(Health..Life.Expectancy. ~ Region, data=data1)
+p6 = boxplot(Trust..Government.Corruption. ~ Region, data=data1)
 
 
 # make a new data frame with only happiness score, region, and year.
@@ -37,13 +37,30 @@ Region.full = Region3_1
 # We found there are still two NA's 
 # They are Taiwan region and Hongkong region
 # so we mannaul put them into Eastern Asia region
-Region.full[is.na(Region.full)] = Region3_1[is.na(Region.full)]
+Region.full[is.na(Region.full)] = Region3_2[is.na(Region.full)]
 Region.full[which(is.na(Region.full))] = "Eastern Asia"
-data3Region = Region.full
+data3$Region = Region.full
 happiness.all$Region = unlist(list(data1$Region, data2$Region, data3$Region))
 
-
+# -------------------------ANOVA--------------------------------
 # parallel boxplot with two factors year and region
-boxplot(happiness.all~yesr*Region, data=happiness.all, xlab='year & region', ylab = 'happiness score') # might switch to ggplot2 for better interpretation.
+p7 = boxplot(score~year*Region, data=happiness.all, xlab='year & region', ylab = 'happiness score') # might switch to ggplot2 for better interpretation.
+# Here we can see that time does not have obvious effects on scores
+# but region does have obvious effects.
+# We need two-way ANOVA to test it. 
+interation.model = aov(score ~ year * Region, data = happiness.all)
+interation.anova = anova(interation.model)
+# The p-value is rounded to 1, so we can conclude there is no interaction effect
+# then we drop the interation term from the model
+addition.model = aov(score~year+Region, data=happiness.all)
+addition.anova = anova(addition.model)
+# the p-value for year is large. -> no major effect of year factor
+# the p-value for Region is small, there is effect of region factor
+
+# Tukey pairwised comparisons
+regionOnly.model = aov(score~Region, data=happiness.all)
+regionOnly.tukey = TukeyHSD(regionOnly.model)
+# We can see if the difference between any pair of regions is statistically significant.
 
 
+# ------------------------linear regression---------------------------------
