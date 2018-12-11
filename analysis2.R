55139190210938a9dbe9140e939b04cf8973ab15
Jack Li <liyunzhejack@gmail.com>
2018-12-11 11:39:10 -0800

    grouping region factors

--- analysis2.R
+++ analysis2.R
@@ -21,6 +21,7 @@
 # ----------------------------------------------------
 
 data = read.csv("./data/standardized_data.csv")
+levels(data$Region) = c("ANZ", "CEE", "EA", "LAC", "MNA", "NA", "SEA", "SA", "SSA", "WE")
 factors.matrix = as.matrix(data[,-c(1,2,3)])
 
 pca.decomp = prcomp(factors.matrix)
@@ -35,9 +36,33 @@ cat("pca model, AIC: ", AIC(pca.lm.model),
      " BIC: ", BIC(pca.lm.model), '\n')
 
 pca.lm.model1 = lm(Score ~ PC1 + PC2, data=pca.table)
-cat("pca model, AIC: ", AIC(pca.lm.model1),
+cat("pca model1, AIC: ", AIC(pca.lm.model1),
      " BIC: ", BIC(pca.lm.model1), '\n')
 
 pca.lm.model2 = lm(Score ~ (PC1 + PC2) * Region, data=pca.table)
 cat("pca model2, AIC: ", AIC(pca.lm.model2),
      " BIC: ", BIC(pca.lm.model2), '\n')
+
+pca.lm.model3 = lm(Score ~ PC1 * Region + PC2, data=pca.table)
+cat("pca model3, AIC: ", AIC(pca.lm.model3),
+     " BIC: ", BIC(pca.lm.model3), '\n')
+
+region.model = aov(Score ~ Region, data = data)
+region.CIs = TukeyHSD(region.model)
+print(which(region.CIs$Region[,4] > 0.05))
+# NOTE:
+# Based on the above Tukey test, I can combine
+# North America, Western Europe, and Australia and New Zeland together
+ggplot(data=pca.table, aes(y=Score, x=PC1, color=Region)) + geom_point() + facet_grid(.~Region)
+
+data.new = pca.table
+# NOTE:
+# NA-ANZ-WE: NAW
+# EA-CEE-MNA-SEA-LAC: ECMSL
+# SSA-SA: SS
+levels(data.new$Region) = c("NAW", "ECMSL", "ECMSL", "ECMSL", "ECMSL", "NAW", "ECMSL", "SS", "SS", "NAW")
+region.model.new = aov(Score ~ Region, data=data.new)
+region.CIs.new = TukeyHSD(region.model.new)
+model.new1 = lm(Score ~ PC1 * Region, data=data.new)
+cat("new model1, AIC: ", AIC(model.new1),
+	" BIC: ", BIC(model.new1), '\n')
